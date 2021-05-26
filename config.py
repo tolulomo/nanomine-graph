a7fed795cd962f366df0f816a0838b5790688dbb
aswallace <aswallace98@gmail.com>
2021-05-26 14:27:46 -0700

    Fix for dataset download storage path bug

--- config.py
+++ config.py
@@ -57,7 +57,8 @@ Config = dict(
     base_rate_probability = 0.8,
 
     nanopub_archive = {
-        #'depot.storage_path' : "/data/nanopublications",
+        'depot.storage_path' : "/data/nanopublications",
+        'cache_max_age' : 3600*24*7,
     },
 
     file_archive = {
